e3e550922a8354674d90fc975720916035b8fcaa
moriisaac <wesongamori@gmail.com>
2022-10-11 00:51:19 +0300

    imagecompressor.py

---
+++ imagecompressor.py
@@ -0,0 +1,33 @@
+'''
+Author: Mori Isaac Wesonga
+Date: 2020-05-05
+Description: This is a simple image compressor
+
+'''
+
+
+
+
+import PIL
+from PIL import Image
+from tkinter.filedialog import *
+
+
+def main():
+    print("\033[32m", "Welcome to the image compressor😇", "\033[0m")
+    compressImage()
+
+def compressImage():
+    # Open the image
+    file = askopenfilename()
+    image = Image.open(file)
+
+    # Compress the image
+    image.save('compressed.jpg',"JPEG", optimize=True, quality=50)
+
+    # Show the image
+    image.show()
+
+
+if __name__ == "__main__":
+    main()
